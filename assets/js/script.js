1ee49e053b2145f760d43e4f2e508bf48460fda5
Kenneth Petiote <Ken@Kens-iMac.local>
2021-04-24 14:02:15 -0400

    Added API Key & Time

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Today's Date using Moment
+var todayDate = moment().format('L');
+
+// Web API Key from OpenWeather API
+var apiKey = "6173ab8c4ac062c85e154afa01ed5a8d";
+
+// List of past search History
+var weatherSearchHistory = [];
+
